662fe0c8e9ad64a2aca583b1f5ee427efb7ee135
bigwps <53157370+bigwps@users.noreply.github.com>
2022-09-19 18:49:06 +0800

    main

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
-package port_scan
+package main
 
 import (
-	"calue"
 	"fmt"
 	"net"
 	"strconv"
 	"time"
-)  
+)
 
 func port_scan() {
 
 	// [...]代表不确定数组个数的情况
 	// make() 函数来创建一个切片 同时创建好相关数组： var slice1 []type =make([]type, len),
 	// 这里  len  是数组的长度并且也是  slice  的初始长度
-	 
+
 	defer_port := [...]int{21, 22, 23, 25, 80, 443, 8080,
 		110, 135, 139, 445, 389, 489, 587, 1433, 1434,
 		1521, 1522, 1723, 2121, 3306, 3389, 4899, 5631,
@@ -25,17 +24,17 @@ func port_scan() {
 	// for - range结构是g特有的迭代结构, for ix, val := range coll { }  ,ix为索引
 	for _, port := range defer_port {
 		//DialTimeout参数1，扫描使用的协议，参数2，IP+端口号，参数3，设置连接超时的时间, strconv.Itoa()将整形转换为字符串
-		_, err := net.DialTimeout("tcp", "127.0.0.1:"+strconv.Itoa(port), 3*time.Second) 
+		_, err := net.DialTimeout("tcp", "127.0.0.1:"+strconv.Itoa(port), 3*time.Second)
 		// 判断是否超时，err是否为空
-		if err == nil{
-			fmt.Printf("端口开放%d\n",port)
+		if err == nil {
+			fmt.Printf("端口开放%d\n", port)
 			res = append(res, port)
-		}else{
-			fmt.Printf("端口关闭%d\n",port)
+		} else {
+			fmt.Printf("端口关闭%d\n", port)
 		}
 	}
 	end := time.Since(start)
-	fmt.Println("花费时间",end)
-	fmt.Println("开放端口",res)
+	fmt.Println("花费时间", end)
+	fmt.Println("开放端口", res)
 
 }
